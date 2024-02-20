f2fb25f946512393d8efd14dba876071dff7cddf
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-20 15:22:30 -0800

    Completed connection module

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.0.177",
+    port: 50541
+  });
+
+  conn.on('connect', () => {
+    console.log('Connected to the server!');
+  });
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+
+module.exports = {
+  connect
+};
