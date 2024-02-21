2cf2822270950751d2575a3d9c41c3b09409dfa7
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-20 17:27:14 -0800

    Completed stdin Setup

--- play.js
+++ play.js
@@ -1,3 +1,19 @@
 const { connect } = require('./client');
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', (handleUserInput) => {
+    if (handleUserInput === '\u0003') {
+      process.exit();
+    }
+  })
+  return stdin;
+};
+
+setupInput();
 connect();
