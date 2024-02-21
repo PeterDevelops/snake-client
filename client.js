2fec087d2116d13520b682d1eea4bff341f77496
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-20 17:05:25 -0800

    Added 'Move: up'

--- client.js
+++ client.js
@@ -9,12 +9,29 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Connected to the server!');
     conn.write('Name: ___');
+  
   });
 
+  // conn.on('connect', () => {
+  //   conn.write('Move: down');
+  // })
+
+  setTimeout(() => {
+    conn.on('connect', () => {
+      conn.write('Move: up');
+    }, 500);
+  });
+
+  // setTimeout(() => {
+  //   conn.on('connect', () => {
+  //     conn.write('Move: down');
+  //   }, 1000);
+  // });
 
   conn.on('data', (data) => {
     console.log(data);
   });
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -27,3 +44,11 @@ console.log("Connecting ...");
 module.exports = {
   connect
 };
+
+/* 
+Note
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)
+*/
