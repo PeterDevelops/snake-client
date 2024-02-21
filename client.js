00253178b65252c4d8f9d98fa974d3a64e0ec424
PeterDevelops <PeterDinhBC@gmail.com>
2024-02-21 12:17:40 -0800

    Added preset keybind for friendly banter

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const net = require("net");
 
-
 const connect = function () {
   const conn = net.createConnection({
     host: "10.0.0.177",
@@ -10,15 +9,12 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Connected to the server!');
     conn.write('Name: ___');
-    
   });
 
   conn.on('data', (data) => {
     console.log(data);
   });
 
-
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   return conn;
--- input.js
+++ input.js
@@ -7,22 +7,37 @@ const setupInput = (conn) => {
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on('data', (handleUserInput) => {
-    if (handleUserInput === 'w') {
-      connection.write('Move: up');
-    }
-    if (handleUserInput === 'a') {
-      connection.write('Move: left');
-    }
-    if (handleUserInput === 's') {
-      connection.write('Move: down');
-    }
-    if (handleUserInput === 'd') {
-      connection.write('Move: right');
-    }
-    if (handleUserInput === '\u0003') {
-      process.exit();
-    }
-    });
+  if (handleUserInput === 'w') {
+    connection.write('Move: up');
+  }
+  if (handleUserInput === 'a') {
+    connection.write('Move: left');
+  }
+  if (handleUserInput === 's') {
+    connection.write('Move: down');
+  }
+  if (handleUserInput === 'd') {
+    connection.write('Move: right');
+  }
+  if (handleUserInput === 'x') {
+    connection.write('Say: Wow!');
+  }
+  if (handleUserInput === 'c') {
+    conn.write('Say: Nice!');
+  }
+  if (handleUserInput === 'v') {
+    conn.write('Say: Naww :P');
+  }
+  if (handleUserInput === 'q') {
+    conn.write('Say: So close!');
+  }
+  if (handleUserInput === 'e') {
+    conn.write('Say: Nice Try!');
+  }
+  if (handleUserInput === '\u0003') {
+    process.exit();
+  }
+  });
 
     return stdin;
 };
